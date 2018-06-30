78438d71f2fa43b383ff634c451741179d93e2cc
Robert Gardner <rgardner@google.com>
2018-06-29 19:59:57 -0700

    Add "createToken" button to CCView Demo.

--- demo-angular/app/item/creditcardview.component.ts
+++ demo-angular/app/item/creditcardview.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { ChangeDetectorRef, Component } from "@angular/core";
 import { CreditCardView, Stripe } from "nativescript-stripe";
 
 @Component({
@@ -9,12 +9,13 @@ import { CreditCardView, Stripe } from "nativescript-stripe";
 export class CreditCardViewComponent {
     token: string;
 
-    constructor() { }
+    constructor(public changeDetectionRef: ChangeDetectorRef) { }
 
     createToken(cardView: CreditCardView): void {
         let stripe = new Stripe("pk_test_s3dHtM9w6XmgB7ak7AbCSj51");
         stripe.createToken(cardView.card, (error, token) => {
             this.token = token.toString() || error;
+            this.changeDetectionRef.detectChanges();
         })
     }
 }
