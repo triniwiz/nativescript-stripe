1c5f3b6164fb978df6ecb87295f97ca7e9bc788a
Robert Gardner <robert@rdgardnerfamily.com>
2018-12-13 08:48:28 -0800

    Remove keys from public repository.

--- demo/app/demo/stripe.service.ts
+++ demo/app/demo/stripe.service.ts
@@ -4,17 +4,17 @@ import { Page } from "ui/page";
 
 // 1) To get started with this demo, first head to https://dashboard.stripe.com/account/apikeys
 // and copy your "Test Publishable Key" (it looks like pk_test_abcdef) into the line below.
-const publishableKey = "pk_test_s3dHtM9w6XmgB7ak7AbCSj51";
+const publishableKey = "";
 
 // 2) Next, optionally, to have this demo save your user's payment details, head to
 // https://github.com/stripe/example-ios-backend , click "Deploy to Heroku", and follow
 // the instructions (don't worry, it's free). Paste your Heroku URL below
 // (it looks like https://blazing-sunrise-1234.herokuapp.com ).
-const backendBaseURL = "https://rg-example-stripe-backend.herokuapp.com/";
+const backendBaseURL = "";
 
 // 3) Optionally, to enable Apple Pay, follow the instructions at https://stripe.com/docs/mobile/apple-pay
 // to create an Apple Merchant ID. Paste it below (it looks like merchant.com.yourappname).
-const appleMerchantID = "merchant.com.hearingclinic.hearingaids";
+const appleMerchantID = "";
 
 export class StripeService implements StripeBackendAPI {
   private customerSession: StripeCustomerSession;
