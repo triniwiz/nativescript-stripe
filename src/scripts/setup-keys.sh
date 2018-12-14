96ba8eab3a80ec9d9a4bd12e976dc8a11c035425
Robert Gardner <robert@rdgardnerfamily.com>
2018-12-13 17:10:34 -0800

    sed options are non-standard across OSs, try perl instead.

--- src/scripts/setup-keys.sh
+++ src/scripts/setup-keys.sh
@@ -1,6 +1,6 @@
-sed -i '' \
-    -e 's/publishableKey = ""/publishableKey = "pk_test_s3dHtM9w6XmgB7ak7AbCSj51"/' \
-    -e 's%backendBaseURL = ""%backendBaseURL = "https://rg-example-stripe-backend.herokuapp.com/"%' \
-    -e 's/appleMerchantID = ""/appleMerchantID = "merchant.com.hearingclinic.hearingaids"/' \
+perl -i -p \
+    -e 's/publishableKey = ""/publishableKey = "pk_test_s3dHtM9w6XmgB7ak7AbCSj51"/;' \
+    -e 's%backendBaseURL = ""%backendBaseURL = "https://rg-example-stripe-backend.herokuapp.com/"%;' \
+    -e 's/appleMerchantID = ""/appleMerchantID = "merchant.com.hearingclinic.hearingaids"/;' \
     ../demo/app/demo/stripe.service.ts \
     ../demo-angular/app/demo/stripe.service.ts
