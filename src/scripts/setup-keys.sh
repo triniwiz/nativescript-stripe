d2abf7825ef6a62a879f227ffb2efc8c24c8ef42
Robert Gardner <robert@rdgardnerfamily.com>
2018-12-15 06:41:59 -0800

    Use test Apple Merchant ID.

--- src/scripts/setup-keys.sh
+++ src/scripts/setup-keys.sh
@@ -1,6 +1,6 @@
 perl -i -p \
     -e 's/publishableKey = ""/publishableKey = "pk_test_s3dHtM9w6XmgB7ak7AbCSj51"/;' \
     -e 's%backendBaseURL = ""%backendBaseURL = "https://rg-example-stripe-backend.herokuapp.com/"%;' \
-    -e 's/appleMerchantID = ""/appleMerchantID = "merchant.com.hearingclinic.hearingaids"/;' \
+    -e 's/appleMerchantID = ""/appleMerchantID = "merchant.com.nativescript-stripe.test"/;' \
     ../demo/app/demo/stripe.service.ts \
     ../demo-angular/app/demo/stripe.service.ts
