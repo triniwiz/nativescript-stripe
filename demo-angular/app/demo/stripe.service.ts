7669786825f778022e83e362369eb698df159bf7
Ryan Pendergast <ryan.pendergast@gmail.com>
2019-04-22 11:37:14 -0500

    Updated demo-angular instructions (#52)

--- demo-angular/app/demo/stripe.service.ts
+++ demo-angular/app/demo/stripe.service.ts
@@ -5,13 +5,13 @@ import { Page } from "tns-core-modules/ui/page";
 
 // 1) To get started with this demo, first head to https://dashboard.stripe.com/account/apikeys
 // and copy your "Test Publishable Key" (it looks like pk_test_abcdef) into the line below.
-const publishableKey = "";
+const publishableKey = "pk_test_yours";
 
 // 2) Next, optionally, to have this demo save your user's payment details, head to
 // https://github.com/stripe/example-ios-backend , click "Deploy to Heroku", and follow
 // the instructions (don't worry, it's free). Paste your Heroku URL below
 // (it looks like https://blazing-sunrise-1234.herokuapp.com ).
-const backendBaseURL = "";
+const backendBaseURL = "https://yours.herokuapp.com/";
 
 // 3) Optionally, to enable Apple Pay, follow the instructions at https://stripe.com/docs/apple-pay/apps
 // to create an Apple Merchant ID. Paste it below (it looks like merchant.com.yourappname).
@@ -22,6 +22,13 @@ export class StripeService implements StripeBackendAPI {
   private customerSession: StripeCustomerSession;
 
   constructor() {
+    if (-1 !== publishableKey.indexOf("pk_test_yours")) {
+      throw new Error("publishableKey must be changed from placeholder");
+    }
+    if (-1 !== backendBaseURL.indexOf("https://yours.herokuapp.com/")) {
+      throw new Error("backendBaseURL must be changed from placeholder");
+    }
+
     StripeConfig.shared().backendAPI = this;
     StripeConfig.shared().publishableKey = publishableKey;
     StripeConfig.shared().appleMerchantID = appleMerchantID;
