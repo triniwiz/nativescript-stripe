647821a55155b9912d0c24a261975236a7b92ba9
Federico Ricchiuto <fed.ricchiuto@gmail.com>
2020-09-04 22:05:39 +0200

    chore: updated demo-angular project
    - generated a brand new project and merged with the old one
    - using the nativescript.config.ts file

--- demo-angular/nativescript.config.ts
+++ demo-angular/nativescript.config.ts
@@ -1,17 +1,10 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'com.demo.NSStripe',
-  main: 'src/main.js',
+  id: 'com.nativescript.StripeDemoNG',
   appResourcesPath: 'App_Resources',
-  webpackConfigPath: 'webpack.config.js',
-  ios: {
-    discardUncaughtJsExceptions: true
-  },
   android: {
-    discardUncaughtJsExceptions: true,
-    v8Flags: '--nolazy --expose_gc',
-    "markingMode": "none",
-    "suppressCallJSMethodExceptions": false
+    v8Flags: '--expose_gc',
+    markingMode: 'none'
   }
 } as NativeScriptConfig;
