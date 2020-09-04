952e3a4df606255fb1a0faed52984aa692788a6c
Federico Ricchiuto <fed.ricchiuto@gmail.com>
2020-09-04 23:53:50 +0200

    fix: removed keys

--- demo-angular/src/app/demo/stripe.service.ts
+++ demo-angular/src/app/demo/stripe.service.ts
@@ -4,13 +4,13 @@ import { Http, Page } from "@nativescript/core";
 
 // 1) To get started with this demo, first head to https://dashboard.stripe.com/account/apikeys
 // and copy your "Test Publishable Key" (it looks like pk_test_abcdef) into the line below.
-export const publishableKey = "pk_test_51H9XU4GWHfmDnQ4lVj91SCAuXPz7iK7VaoDz3lMhzRgBiDW8lUbVfecLdesJksaF2ETgJlQVNeh5OTYbPnsrbXr200Be9DN0CP";
+export const publishableKey = "pk_test_yours";
 
 // 2) Next, optionally, to have this demo save your user's payment details, head to
 // https://github.com/stripe/example-ios-backend , click "Deploy to Heroku", and follow
 // the instructions (don't worry, it's free). Paste your Heroku URL below
 // (it looks like https://blazing-sunrise-1234.herokuapp.com ).
-const backendBaseURL = "https://xxx.herokuapp.com/";
+const backendBaseURL = "https://yours.herokuapp.com/";
 
 // 3) Optionally, to enable Apple Pay, follow the instructions at https://stripe.com/docs/apple-pay/apps
 // to create an Apple Merchant ID. Paste it below (it looks like merchant.com.yourappname).
