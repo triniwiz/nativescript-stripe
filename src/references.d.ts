558c6e403123384d87da81857d30b2a306585ac9
Federico Ricchiuto <fed.ricchiuto@gmail.com>
2020-09-05 10:55:29 +0200

    fix: aligned android typings with minSdkVersion

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,1 +1,3 @@
-/// <reference path="./node_modules/@nativescript/types/index.d.ts" />
+/// <reference path="./node_modules/@nativescript/core/global-types.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/lib/android-19.d.ts" />
